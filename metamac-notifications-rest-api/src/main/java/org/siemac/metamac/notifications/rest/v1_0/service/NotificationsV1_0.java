c2843390800cd0d4220cbf1d057454680a5ce82c
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-10-31 13:40:42 +0000

    METAMAC-1967: Api Rest de notificaciones http://jira.arte-consultores.com/browse/METAMAC-1967
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@49806 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-notifications-rest-api/src/main/java/org/siemac/metamac/notifications/rest/v1_0/service/NotificationsV1_0.java
@@ -0,0 +1,9 @@
+package org.siemac.metamac.notifications.rest.v1_0.service;
+
+import javax.ws.rs.Path;
+
+@Path("v1.0")
+// IMPORTANT: If a new version of API is added, remember change latest url y urlrewrite.xml in war
+public interface NotificationsV1_0 {
+
+}
