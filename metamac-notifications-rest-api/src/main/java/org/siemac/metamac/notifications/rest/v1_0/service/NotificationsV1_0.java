742d481718f701b8052a298cce54ddf1a9e24e7f
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-04 14:05:27 +0000

    METAMAC-1967: Api Rest de notificaciones http://jira.arte-consultores.com/browse/METAMAC-1967 METAMAC-1962: Proyecto de notificaciones http://jira.arte-consultores.com/browse/METAMAC-1962
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@49828 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-api/src/main/java/org/siemac/metamac/notifications/rest/v1_0/service/NotificationsV1_0.java
+++ metamac-notifications-rest-api/src/main/java/org/siemac/metamac/notifications/rest/v1_0/service/NotificationsV1_0.java
@@ -1,9 +1,27 @@
 package org.siemac.metamac.notifications.rest.v1_0.service;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.siemac.metamac.rest.notifications.v1_0.domain.Notification;
 
 @Path("v1.0")
 // IMPORTANT: If a new version of API is added, remember change latest url y urlrewrite.xml in war
 public interface NotificationsV1_0 {
 
+    @PUT
+    @Consumes({"application/xml"})
+    @Produces({"application/xml"})
+    @Path("notifications")
+    Response createNotification(Notification notification);
+
+    @GET
+    @Produces({"application/xml"})
+    @Path("notifications/{urn}")
+    Notification retrieveResourceByUrn(@PathParam("urn") String urn);
 }
