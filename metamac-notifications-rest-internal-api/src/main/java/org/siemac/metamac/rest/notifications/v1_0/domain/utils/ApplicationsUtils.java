050f16e09d2e9dd20d0e79fd488ba44b38268a41
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-12-19 08:35:19 +0000

    METAMAC-2008 - [API] Generar utilidades para poder construir con facilidad las entidades de la API
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50406 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-api/src/main/java/org/siemac/metamac/rest/notifications/v1_0/domain/utils/ApplicationsUtils.java
+++ metamac-notifications-rest-internal-api/src/main/java/org/siemac/metamac/rest/notifications/v1_0/domain/utils/ApplicationsUtils.java
@@ -1,13 +1,14 @@
 package org.siemac.metamac.rest.notifications.v1_0.domain.utils;
 
 import org.siemac.metamac.rest.notifications.v1_0.domain.Applications;
+import org.siemac.metamac.rest.notifications.v1_0.domain.enume.MetamacApplicationsEnum;
 
 public class ApplicationsUtils {
 
-    public static Applications createApplicationsList(String... applicationsCodes) {
+    public static Applications createApplicationsList(MetamacApplicationsEnum... applicationsCodes) {
         Applications applications = new Applications();
-        for (String appName : applicationsCodes) {
-            applications.getApplications().add(ApplicationBuilder.application().withName(appName).build());
+        for (MetamacApplicationsEnum appName : applicationsCodes) {
+            applications.getApplications().add(ApplicationBuilder.application().withName(appName.toString()).build());
         }
         return applications;
     }
--- metamac-notifications-rest-internal-api/src/main/java/org/siemac/metamac/rest/notifications/v1_0/domain/utils/RolesUtils.java
+++ metamac-notifications-rest-internal-api/src/main/java/org/siemac/metamac/rest/notifications/v1_0/domain/utils/RolesUtils.java
@@ -1,13 +1,14 @@
 package org.siemac.metamac.rest.notifications.v1_0.domain.utils;
 
 import org.siemac.metamac.rest.notifications.v1_0.domain.Roles;
+import org.siemac.metamac.rest.notifications.v1_0.domain.enume.MetamacRolesEnum;
 
 public class RolesUtils {
 
-    public static Roles createRolesList(String... rolesCodes) {
+    public static Roles createRolesList(MetamacRolesEnum... rolesCodes) {
         Roles roles = new Roles();
-        for (String roleName : rolesCodes) {
-            roles.getRoles().add(RoleBuilder.role().withName(roleName).build());
+        for (MetamacRolesEnum roleName : rolesCodes) {
+            roles.getRoles().add(RoleBuilder.role().withName(roleName.toString()).build());
         }
         return roles;
     }
