90ea18ec87c9f38c557d5d14b7f6bb222468354c
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-09 14:22:03 +0000

    METAMAC-2046 - Añadir la Suite de tests http://jira.arte-consultores.com/browse/METAMAC-2046
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50538 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-core/src/test/java/org/siemac/metamac/notifications/core/NotificationsSuite.java
+++ metamac-notifications-core/src/test/java/org/siemac/metamac/notifications/core/NotificationsSuite.java
@@ -2,10 +2,17 @@ package org.siemac.metamac.notifications.core;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.siemac.metamac.notifications.core.channel.mail.serviceimpl.MailChannelServiceTest;
+import org.siemac.metamac.notifications.core.common.repositoryimpl.InternationalStringRepositoryTest;
+import org.siemac.metamac.notifications.core.notice.repositoryimpl.NotificationRepositoryTest;
+import org.siemac.metamac.notifications.core.notice.serviceapi.NotificationServiceTest;
 
 // @formatter:off
 @RunWith(Suite.class)
-@Suite.SuiteClasses({/*NotificationsServiceFacadeTest.class*/})
+@Suite.SuiteClasses({MailChannelServiceTest.class,
+                        InternationalStringRepositoryTest.class,
+                        NotificationRepositoryTest.class,
+                        NotificationServiceTest.class})
 // @formatter:on
 public class NotificationsSuite {
 }
