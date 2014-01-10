dd36b215608c462cad06ff7db1d1bb10e0a014ea
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-10 12:36:19 +0000

    METAMAC-2051 - Añadir Suite de Tests al proyecto api-impl http://jira.arte-consultores.com/browse/METAMAC-2051
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50557 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/NotificationsRestInternalSuite.java
@@ -0,0 +1,12 @@
+package org.siemac.metamac.rest.notifications.v1_0;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.siemac.metamac.rest.notifications.v1_0.service.NotificationsRestInternalFacadeV10NotificationsTest;
+
+// @formatter:off
+@RunWith(Suite.class)
+@Suite.SuiteClasses({NotificationsRestInternalFacadeV10NotificationsTest.class})
+// @formatter:on
+public class NotificationsRestInternalSuite {
+}
