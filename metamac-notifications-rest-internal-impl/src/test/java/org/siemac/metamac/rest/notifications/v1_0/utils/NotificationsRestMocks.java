e2acb3bde2d3cd07d7388d92374dfce5b00a7512
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-14 13:09:30 +0000

    METAMAC-2063 - Simplificar la creación de los mocks rest haciendo uso de las utilidades de la interfaz de la API http://jira.arte-consultores.com/browse/METAMAC-2063
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50606 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestMocks.java
+++ metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestMocks.java
@@ -1,65 +1,45 @@
 package org.siemac.metamac.rest.notifications.v1_0.utils;
 
-import java.math.BigInteger;
 import java.util.Date;
 
-import org.siemac.metamac.rest.notifications.v1_0.domain.Message;
 import org.siemac.metamac.rest.notifications.v1_0.domain.Messages;
 import org.siemac.metamac.rest.notifications.v1_0.domain.Notification;
 import org.siemac.metamac.rest.notifications.v1_0.domain.NotificationType;
-import org.siemac.metamac.rest.notifications.v1_0.domain.Receiver;
 import org.siemac.metamac.rest.notifications.v1_0.domain.Receivers;
-import org.siemac.metamac.rest.notifications.v1_0.domain.Role;
 import org.siemac.metamac.rest.notifications.v1_0.domain.Roles;
 import org.siemac.metamac.rest.notifications.v1_0.domain.StatisticalOperations;
+import org.siemac.metamac.rest.notifications.v1_0.domain.enume.MetamacRolesEnum;
+import org.siemac.metamac.rest.notifications.v1_0.domain.utils.MessagesUtils;
+import org.siemac.metamac.rest.notifications.v1_0.domain.utils.ReceiversUtils;
+import org.siemac.metamac.rest.notifications.v1_0.domain.utils.RolesUtils;
+import org.siemac.metamac.rest.notifications.v1_0.domain.utils.StatisticalOperationsUtils;
 
 public class NotificationsRestMocks {
 
     public static Notification mockNotification_TYPE_NOTIFICATION() {
         Notification notification = new Notification();
+        notification.setNotificationType(NotificationType.NOTIFICATION);
 
         notification.setSendingApplication("application");
         notification.setSendingUser("user");
         notification.setExpirationDate(new Date());
         notification.setSubject("My subject");
 
-        {
-            // ROLES
-            Roles roles = new Roles();
-            Role role = new Role();
-            role.setName("ADMIN");
-            roles.getRoles().add(role);
-            roles.setTotal(new BigInteger(String.valueOf(roles.getRoles().size())));
-            notification.setRoles(roles);
-        }
+        // Roles
+        Roles roles = RolesUtils.createRolesList(MetamacRolesEnum.ADMINISTRADOR);
+        notification.setRoles(roles);
 
-        {
-            // STATISTICAL OPERATIONS
-            StatisticalOperations statisticalOperations = new StatisticalOperations();
-            org.siemac.metamac.rest.notifications.v1_0.domain.StatisticalOperation statisticalOperation = new org.siemac.metamac.rest.notifications.v1_0.domain.StatisticalOperation();
-            statisticalOperation.setName("urn:siemac:org.siemac.metamac.infomodel.statisticaloperations.Operation=operation01");
-            statisticalOperations.getStatisticalOperations().add(statisticalOperation);
-            statisticalOperations.setTotal(new BigInteger(String.valueOf(statisticalOperations.getStatisticalOperations().size())));
-            notification.setStatisticalOperations(statisticalOperations);
-        }
-
-        // Messages
-        Messages messages = new Messages();
-        Message message = new Message();
-        message.setText("My message");
-        messages.getMessages().add(message);
-
-        notification.setNotificationType(NotificationType.NOTIFICATION);
+        // Statistical operations
+        StatisticalOperations statisticalOperations = StatisticalOperationsUtils.createStatisticalOperationsList("urn:siemac:org.siemac.metamac.infomodel.statisticaloperations.Operation=operation01");
+        notification.setStatisticalOperations(statisticalOperations);
 
         // Receivers
-        Receivers receivers = new Receivers();
-        receivers.setTotal(new BigInteger("5"));
-        for (int i = 0; i < 5; i++) {
-            Receiver receiver = new Receiver();
-            receiver.setUsername("user-" + i);
-            receivers.getReceivers().add(receiver);
-        }
+        Receivers receivers = ReceiversUtils.createReceiversList("user-1", "user-2", "user-3", "user-4", "user-5");
         notification.setReceivers(receivers);
+
+        // Messages
+        Messages messages = MessagesUtils.createMessagesList("My message");
+        notification.setMessages(messages);
 
         return notification;
     }
