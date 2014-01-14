1b1d313e786f60cd68b0f68f8737f01509ee30e4
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-14 17:18:00 +0000

    METAMAC-2059 - Simplificar la creación de mocks de la api para que se reutilicen los del CORE cuando se necesiten entidades http://jira.arte-consultores.com/browse/METAMAC-2059
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50613 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestDoMocks.java
+++ metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestDoMocks.java
@@ -1,19 +1,28 @@
 package org.siemac.metamac.rest.notifications.v1_0.utils;
 
+import org.siemac.metamac.notifications.core.notice.domain.Message;
 import org.siemac.metamac.notifications.core.notice.domain.Notification;
 import org.siemac.metamac.statistical.resources.core.utils.sql.NotificationsDoMocks;
 
 public class NotificationsRestDoMocks {
 
     public static Notification mockNotification_TYPE_NOTIFICATION(String urn) {
-        Notification notification = buildNotificationWithResourcesTypeNotification();
+        Notification notification = buildNotificationWithoutResourcesTypeNotification();
         notification.setUrn(urn);
 
         return notification;
     }
 
-    private static Notification buildNotificationWithResourcesTypeNotification() {
-        Notification notification = NotificationsDoMocks.mockNotificationWithResources_TYPE_NOTIFICATION();
+    private static Notification buildNotificationWithoutResourcesTypeNotification() {
+        Notification notification = NotificationsDoMocks.mockNotificationWithoutResources_TYPE_NOTIFICATION();
+
+        // Subject
+        notification.setSubject("My subject");
+
+        // Message
+        notification.setSendingApplication("TEST-APPLICATION");
+        notification.getMessages().clear();
+        notification.addMessage(new Message("My message"));
         return notification;
     }
 
