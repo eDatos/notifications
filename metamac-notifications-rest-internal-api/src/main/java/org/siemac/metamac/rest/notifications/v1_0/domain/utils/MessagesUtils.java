d1aaf12d03ee0364db51a63242af11fa57ad6639
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-14 13:04:51 +0000

    METAMAC-2062 - Añadir utilidades de creación de mensajes a la interfaz de la API http://jira.arte-consultores.com/browse/METAMAC-2062
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50605 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-notifications-rest-internal-api/src/main/java/org/siemac/metamac/rest/notifications/v1_0/domain/utils/MessagesUtils.java
@@ -0,0 +1,18 @@
+package org.siemac.metamac.rest.notifications.v1_0.domain.utils;
+
+import java.math.BigInteger;
+
+import org.siemac.metamac.rest.notifications.v1_0.domain.Messages;
+
+public class MessagesUtils {
+
+    public static Messages createMessagesList(String... messagesTexts) {
+        Messages messages = new Messages();
+        for (String text : messagesTexts) {
+            messages.getMessages().add(MessageBuilder.message().withText(text).build());
+        }
+
+        messages.setTotal(new BigInteger(String.valueOf(messages.getMessages().size())));
+        return messages;
+    }
+}
