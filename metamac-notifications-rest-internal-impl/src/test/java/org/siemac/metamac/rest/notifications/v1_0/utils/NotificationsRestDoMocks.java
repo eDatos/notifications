e270cc650d38978c496b6295b73d53f408b5b0cc
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-14 10:06:19 +0000

    >> METAMAC-2058 - Añadir asunto a las notificaciones http://jira.arte-consultores.com/browse/METAMAC-2058
    >> METAMAC-2059 - Simplificar la creación de mocks de la api para que se reutilicen los del CORE cuando se necesiten entidades
    http://jira.arte-consultores.com/browse/METAMAC-2059
    
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50594 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestDoMocks.java
+++ metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestDoMocks.java
@@ -1,27 +1,17 @@
 package org.siemac.metamac.rest.notifications.v1_0.utils;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-
-import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
-import org.siemac.metamac.common.test.utils.MetamacMocks;
-import org.siemac.metamac.core.common.constants.CoreCommonConstants;
-import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
-import org.siemac.metamac.core.common.enume.utils.TypeExternalArtefactsEnumUtils;
-import org.siemac.metamac.notifications.core.common.domain.ExternalItem;
-import org.siemac.metamac.notifications.core.common.domain.InternationalString;
-import org.siemac.metamac.notifications.core.common.domain.LocalisedString;
 import org.siemac.metamac.notifications.core.notice.domain.Message;
 import org.siemac.metamac.notifications.core.notice.domain.Notification;
 import org.siemac.metamac.notifications.core.notice.domain.Receiver;
 import org.siemac.metamac.notifications.core.notice.domain.Role;
 import org.siemac.metamac.notifications.core.notice.enume.domain.NotificationType;
+import org.siemac.metamac.statistical.resources.core.utils.sql.NotificationsDoMocks;
 
 public class NotificationsRestDoMocks {
 
     public static Notification mockNotification_TYPE_NOTIFICATION() {
-        Notification notification = new Notification("application", NotificationType.NOTIFICATION);
+        Notification notification = new Notification("application", "My subject", NotificationType.NOTIFICATION);
         notification.setExpirationDate(new DateTime(2013, 1, 1, 1, 1, 1, 1));
         notification.getMessages().add(new Message("My message"));
 
@@ -34,7 +24,7 @@ public class NotificationsRestDoMocks {
 
         {
             // statistical operation
-            notification.addStatisticalOperation(mockStatisticalOperationExternalItem("operation01"));
+            notification.addStatisticalOperation(NotificationsDoMocks.mockStatisticalOperationExternalItem("operation01"));
         }
 
         // Receivers
@@ -54,86 +44,4 @@ public class NotificationsRestDoMocks {
         return notification;
     }
 
-    public static ExternalItem mockStatisticalOperationExternalItem(String code) {
-        return mockExternalItem(code, MetamacMocks.mockStatisticalOperationUrn(code), TypeExternalArtefactsEnum.STATISTICAL_OPERATION);
-    }
-
-    private static ExternalItem mockExternalItem(String code, String urn, TypeExternalArtefactsEnum type) {
-        return mockExternalItem(code, null, urn, type);
-    }
-
-    private static ExternalItem mockExternalItem(String code, String codeNested, String uri, String urnProvider, String urn, TypeExternalArtefactsEnum type, InternationalString title,
-            String managementAppUrl) {
-        ExternalItem target = mockExternalItem(code, codeNested, uri, urnProvider, urn, type);
-        target.setTitle(title);
-        target.setManagementAppUrl(managementAppUrl);
-        return target;
-    }
-
-    private static ExternalItem mockExternalItem(String code, String codeNested, String uri, String urnProvider, String urn, TypeExternalArtefactsEnum type) {
-        ExternalItem target = new ExternalItem();
-        target.setVersion(Long.valueOf(0));
-        target.setCode(code);
-        target.setCodeNested(codeNested);
-        target.setUri(uri);
-        target.setUrn(urn);
-        target.setUrnProvider(urnProvider);
-        target.setType(type);
-        return target;
-    }
-
-    private static ExternalItem mockExternalItem(String code, String codeNested, String urn, TypeExternalArtefactsEnum type) {
-        String uri = CoreCommonConstants.API_LATEST_WITH_SLASHES + code;
-        String urnProvider = urn + ":provider";
-        InternationalString title = mockInternationalStringMetadata(code, "title");
-        String managementAppUrl = CoreCommonConstants.URL_SEPARATOR + code;
-
-        if (TypeExternalArtefactsEnumUtils.isExternalItemOfCommonMetadataApp(type)) {
-            urnProvider = null;
-        } else if (TypeExternalArtefactsEnumUtils.isExternalItemOfStatisticalOperationsApp(type)) {
-            urnProvider = null;
-        } else if (TypeExternalArtefactsEnumUtils.isExternalItemOfSrmApp(type)) {
-            // nothing to do with urnInternal because it's ok for SrmExternalItems
-            if (StringUtils.isBlank(codeNested)) {
-                if (TypeExternalArtefactsEnum.AGENCY.equals(type) || TypeExternalArtefactsEnum.CATEGORY.equals(type)) {
-                    codeNested = code;
-                }
-            }
-        } else {
-            fail("Unexpected type of ExternalItem:" + type);
-        }
-
-        ExternalItem item = mockExternalItem(code, codeNested, uri, urnProvider, urn, type, title, managementAppUrl);
-        return item;
-    }
-
-    private static InternationalString mockInternationalStringMetadata(String resource, String metadata) {
-        assertNotNull(metadata);
-
-        InternationalString internationalString = new InternationalString();
-        internationalString.setVersion(Long.valueOf(0));
-        {
-            LocalisedString es = new LocalisedString();
-            if (resource != null) {
-                es.setLabel(metadata + "-" + resource + " en Espanol");
-            } else {
-                es.setLabel(metadata + " en Espanol");
-            }
-            es.setLocale("es");
-            es.setVersion(Long.valueOf(0));
-            internationalString.addText(es);
-        }
-        {
-            LocalisedString en = new LocalisedString();
-            if (resource != null) {
-                en.setLabel(metadata + "-" + resource + " in English");
-            } else {
-                en.setLabel(metadata + " in English");
-            }
-            en.setLocale("en");
-            en.setVersion(Long.valueOf(0));
-            internationalString.addText(en);
-        }
-        return internationalString;
-    }
 }
