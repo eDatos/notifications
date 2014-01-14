2e92e360ceca184786dc459a5869466178b05e57
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-14 10:38:56 +0000

    METAMAC-2059 - Simplificar la creación de mocks de la api para que se reutilicen los del CORE cuando se necesiten entidades http://jira.arte-consultores.com/browse/METAMAC-2059
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50599 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestDoMocks.java
+++ metamac-notifications-rest-internal-impl/src/test/java/org/siemac/metamac/rest/notifications/v1_0/utils/NotificationsRestDoMocks.java
@@ -1,46 +1,19 @@
 package org.siemac.metamac.rest.notifications.v1_0.utils;
 
-import org.joda.time.DateTime;
-import org.siemac.metamac.notifications.core.notice.domain.Message;
 import org.siemac.metamac.notifications.core.notice.domain.Notification;
-import org.siemac.metamac.notifications.core.notice.domain.Receiver;
-import org.siemac.metamac.notifications.core.notice.domain.Role;
-import org.siemac.metamac.notifications.core.notice.enume.domain.NotificationType;
 import org.siemac.metamac.statistical.resources.core.utils.sql.NotificationsDoMocks;
 
 public class NotificationsRestDoMocks {
 
-    public static Notification mockNotification_TYPE_NOTIFICATION() {
-        Notification notification = new Notification("application", "My subject", NotificationType.NOTIFICATION);
-        notification.setExpirationDate(new DateTime(2013, 1, 1, 1, 1, 1, 1));
-        notification.getMessages().add(new Message("My message"));
-
-        {
-            // role
-            Role role = new Role();
-            role.setName("ADMIN");
-            notification.addRole(role);
-        }
-
-        {
-            // statistical operation
-            notification.addStatisticalOperation(NotificationsDoMocks.mockStatisticalOperationExternalItem("operation01"));
-        }
-
-        // Receivers
-        for (int i = 0; i < 5; i++) {
-            Receiver receiverElement = new Receiver();
-            receiverElement.setUsername("user-" + i);
-            notification.addReceiver(receiverElement);
-        }
+    public static Notification mockNotification_TYPE_NOTIFICATION(String urn) {
+        Notification notification = buildNotificationWithResourcesTypeNotification();
+        notification.setUrn(urn);
 
         return notification;
     }
 
-    public static Notification mockNotification_TYPE_NOTIFICATION(String urn) {
-        Notification notification = mockNotification_TYPE_NOTIFICATION();
-        notification.setUrn(urn);
-
+    private static Notification buildNotificationWithResourcesTypeNotification() {
+        Notification notification = NotificationsDoMocks.mockNotificationWithResources_TYPE_NOTIFICATION();
         return notification;
     }
 
