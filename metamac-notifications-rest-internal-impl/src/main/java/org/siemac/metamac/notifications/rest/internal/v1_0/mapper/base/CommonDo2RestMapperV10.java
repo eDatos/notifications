d008a3ba33cfc004062b58e0e694db3e27345272
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-24 15:26:53 +0000

    METAMAC-2049 - Las notificaciones deben poder tener una lista de mensajes y además los mensajes pueden tener asociados listas de recursos
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50864 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-impl/src/main/java/org/siemac/metamac/notifications/rest/internal/v1_0/mapper/base/CommonDo2RestMapperV10.java
+++ metamac-notifications-rest-internal-impl/src/main/java/org/siemac/metamac/notifications/rest/internal/v1_0/mapper/base/CommonDo2RestMapperV10.java
@@ -2,14 +2,25 @@ package org.siemac.metamac.notifications.rest.internal.v1_0.mapper.base;
 
 import java.util.List;
 
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.notifications.core.common.domain.ExternalItem;
+import org.siemac.metamac.rest.common.v1_0.domain.InternationalString;
 import org.siemac.metamac.rest.common.v1_0.domain.Resource;
+import org.siemac.metamac.rest.common.v1_0.domain.ResourceLink;
 import org.siemac.metamac.rest.common.v1_0.domain.Resources;
 
 public interface CommonDo2RestMapperV10 {
 
-    public Resources toResourcesExternalItemsStatisticalOperations(List<ExternalItem> sources, List<String> selectedLanguages);
+    public Resources externalItemEntityListToRest(List<ExternalItem> source) throws MetamacException;
 
-    public Resource toResourceExternalItemStatisticalOperations(ExternalItem source, List<String> selectedLanguages);
+    public Resource externalItemEntityToRest(ExternalItem source) throws MetamacException;
+
+    public InternationalString stringEntityToInternationalStringRest(String source);
+
+    public InternationalString internationalStringEntityToRest(InternationalString sources);
+
+    public InternationalString internationalStringEntityToRest(org.siemac.metamac.notifications.core.common.domain.InternationalString sources);
+
+    public ResourceLink uriToResourceLink(String kind, String href);
 
 }
--- metamac-notifications-rest-internal-impl/src/main/java/org/siemac/metamac/notifications/rest/internal/v1_0/mapper/base/CommonDo2RestMapperV10Impl.java
+++ metamac-notifications-rest-internal-impl/src/main/java/org/siemac/metamac/notifications/rest/internal/v1_0/mapper/base/CommonDo2RestMapperV10Impl.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.siemac.metamac.core.common.conf.ConfigurationService;
+import org.siemac.metamac.core.common.enume.utils.TypeExternalArtefactsEnumUtils;
+import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.notifications.core.common.domain.ExternalItem;
 import org.siemac.metamac.rest.common.v1_0.domain.InternationalString;
 import org.siemac.metamac.rest.common.v1_0.domain.LocalisedString;
@@ -24,7 +25,12 @@ public class CommonDo2RestMapperV10Impl implements CommonDo2RestMapperV10 {
     @Autowired
     private ConfigurationService configurationService;
 
+    private String               statisticalResourcesApiInternalEndpoint;
+    private String               srmApiInternalEndpoint;
     private String               statisticalOperationsApiInternalEndpoint;
+    private String               commonMetadataApiExternalEndpoint;
+
+    private String               defaultLanguage;
 
     @PostConstruct
     public void init() throws Exception {
@@ -33,64 +39,152 @@ public class CommonDo2RestMapperV10Impl implements CommonDo2RestMapperV10 {
         // Statistical operations internal Api (do not add api version! it is already stored in database (~latest))
         statisticalOperationsApiInternalEndpoint = configurationService.retrieveStatisticalOperationsInternalApiUrlBase();
         statisticalOperationsApiInternalEndpoint = StringUtils.removeEnd(statisticalOperationsApiInternalEndpoint, "/");
+
+        // SRM external Api (do not add api version! it is already stored in database (~latest))
+        srmApiInternalEndpoint = configurationService.retrieveSrmInternalApiUrlBase();
+        srmApiInternalEndpoint = StringUtils.removeEnd(srmApiInternalEndpoint, "/");
+
+        // Statistical resources internal API (do not add api version! it is already stored in database (~latest))
+        // FIXME: Cambiar internal por internal. Pendiente de que este la API Interna.
+        statisticalResourcesApiInternalEndpoint = configurationService.retrieveStatisticalResourcesExternalApiUrlBase();
+        statisticalResourcesApiInternalEndpoint = StringUtils.removeEnd(statisticalResourcesApiInternalEndpoint, "/");;
+
+        // Common metadata external Api (do not add api version! it is already stored in database (~latest))
+        commonMetadataApiExternalEndpoint = configurationService.retrieveCommonMetadataExternalApiUrlBase();
+        commonMetadataApiExternalEndpoint = StringUtils.removeEnd(commonMetadataApiExternalEndpoint, "/");;
+
+        // MISC
+        defaultLanguage = configurationService.retrieveLanguageDefault();
     }
 
+    // ------------------------------------------------------------
+    // EXTERNAL ITEM
+    // ------------------------------------------------------------
+
     @Override
-    public Resources toResourcesExternalItemsStatisticalOperations(List<ExternalItem> sources, List<String> selectedLanguages) {
-        if (CollectionUtils.isEmpty(sources)) {
+    public Resources externalItemEntityListToRest(List<ExternalItem> source) throws MetamacException {
+        if (source.isEmpty()) {
             return null;
         }
-        Resources targets = new Resources();
-        for (ExternalItem source : sources) {
-            Resource target = toResourceExternalItemStatisticalOperations(source, selectedLanguages);
-            targets.getResources().add(target);
+
+        Resources result = new Resources();
+        for (ExternalItem externalItem : source) {
+            result.getResources().add(externalItemEntityToRest(externalItem));
         }
-        targets.setTotal(BigInteger.valueOf(targets.getResources().size()));
-        return targets;
+        result.setTotal(new BigInteger(String.valueOf(result.getResources().size())));
+        return result;
     }
 
     @Override
-    public Resource toResourceExternalItemStatisticalOperations(ExternalItem source, List<String> selectedLanguages) {
+    public Resource externalItemEntityToRest(ExternalItem source) throws MetamacException {
+        Resource target = externalItemEntityToRestWithoutUrls(source);
+
+        if (target != null) {
+            if (TypeExternalArtefactsEnumUtils.isExternalItemOfCommonMetadataApp(source.getType())) {
+                target = commonMetadataExternalItemEntityToRest(source, target);
+            } else if (TypeExternalArtefactsEnumUtils.isExternalItemOfSrmApp(source.getType())) {
+                target = srmExternalItemEntityToRest(source, target);
+            } else if (TypeExternalArtefactsEnumUtils.isExternalItemOfStatisticalOperationsApp(source.getType())) {
+                target = statisticalOperationsExternalItemEntityToRest(source, target);
+                // FIXME: Añadir tipos de external item del gpe
+                // } else if (TypeExternalArtefactsEnumUtils.isExternalItemOfStatisticalResourcesApp(source.getType())) {
+                // target = statisticalResourcesExternalItemEntityToRest(source, target);
+            } else {
+                throw new MetamacException(org.siemac.metamac.notifications.core.error.ServiceExceptionType.UNKNOWN, "Type of externalItem not defined for externalItemEntityToRest");
+            }
+        }
+
+        return target;
+    }
+
+    private Resource externalItemEntityToRestWithoutUrls(ExternalItem source) {
         if (source == null) {
             return null;
         }
         Resource target = new Resource();
-        toResourceExternalItem(source, statisticalOperationsApiInternalEndpoint, target, selectedLanguages);
-        return target;
-    }
-
-    private void toResourceExternalItem(ExternalItem source, String apiExternalItemBase, Resource target, List<String> selectedLanguages) {
-        if (source == null) {
-            return;
-        }
         target.setId(source.getCode());
         target.setNestedId(source.getCodeNested());
         target.setUrn(source.getUrn());
         target.setKind(source.getType().getValue());
-        target.setSelfLink(toResourceLink(target.getKind(), RestUtils.createLink(apiExternalItemBase, source.getUri())));
-        target.setName(toInternationalString(source.getTitle(), selectedLanguages));
+        target.setName(internationalStringEntityToRest(source.getTitle()));
+
+        return target;
     }
 
-    private ResourceLink toResourceLink(String kind, String href) {
+    private Resource statisticalOperationsExternalItemEntityToRest(ExternalItem source, Resource target) {
+        target.setSelfLink(uriToResourceLink(target.getKind(), RestUtils.createLink(statisticalOperationsApiInternalEndpoint, source.getUri())));
+        return target;
+    }
+
+    private Resource srmExternalItemEntityToRest(ExternalItem source, Resource target) {
+        target.setSelfLink(uriToResourceLink(target.getKind(), RestUtils.createLink(srmApiInternalEndpoint, source.getUri())));
+        return target;
+    }
+
+    private Resource commonMetadataExternalItemEntityToRest(ExternalItem source, Resource target) {
+        target.setSelfLink(uriToResourceLink(target.getKind(), RestUtils.createLink(commonMetadataApiExternalEndpoint, source.getUri())));
+        return target;
+    }
+
+    private Resource statisticalResourcesExternalItemEntityToRest(ExternalItem source, Resource target) {
+        target.setSelfLink(uriToResourceLink(target.getKind(), RestUtils.createLink(statisticalResourcesApiInternalEndpoint, source.getUri())));
+        return target;
+    }
+
+    // ------------------------------------------------------------
+    // RESOURCE LINKS
+    // ------------------------------------------------------------
+
+    @Override
+    public ResourceLink uriToResourceLink(String kind, String href) {
         ResourceLink target = new ResourceLink();
         target.setKind(kind);
         target.setHref(href);
         return target;
     }
 
-    public InternationalString toInternationalString(org.siemac.metamac.notifications.core.common.domain.InternationalString sources, List<String> selectedLanguages) {
+    // ------------------------------------------------------------
+    // INTERNATIONAL STRINGS
+    // ------------------------------------------------------------
+
+    @Override
+    public InternationalString internationalStringEntityToRest(org.siemac.metamac.notifications.core.common.domain.InternationalString sources) {
         if (sources == null) {
             return null;
         }
         InternationalString targets = new InternationalString();
         for (org.siemac.metamac.notifications.core.common.domain.LocalisedString source : sources.getTexts()) {
-            if (selectedLanguages.isEmpty() || selectedLanguages.contains(source.getLocale())) {
-                LocalisedString target = new LocalisedString();
-                target.setLang(source.getLocale());
-                target.setValue(source.getLabel());
-                targets.getTexts().add(target);
-            }
+            LocalisedString target = new LocalisedString();
+            target.setLang(source.getLocale());
+            target.setValue(source.getLabel());
+            targets.getTexts().add(target);
         }
         return targets;
     }
+
+    @Override
+    public InternationalString internationalStringEntityToRest(InternationalString sources) {
+        if (sources == null) {
+            return null;
+        }
+        InternationalString targets = new InternationalString();
+        for (LocalisedString source : sources.getTexts()) {
+            targets.getTexts().add(source);
+        }
+        return targets;
+    }
+
+    @Override
+    public InternationalString stringEntityToInternationalStringRest(String source) {
+        if (source == null) {
+            return null;
+        }
+        InternationalString targets = new InternationalString();
+        LocalisedString target = new LocalisedString();
+        target.setLang(defaultLanguage);
+        target.setValue(source);
+        targets.getTexts().add(target);
+        return targets;
+    }
+
 }
