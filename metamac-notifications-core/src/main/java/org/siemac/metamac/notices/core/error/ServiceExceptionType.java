1cd240d7120708d9dcce4c9a5fdf6f4f6c94996c
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-29 10:18:47 +0000

    METAMAC-2103 - [CORE] [API] Refactor notification por notices
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@50920 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-core/src/main/java/org/siemac/metamac/notices/core/error/ServiceExceptionType.java
+++ metamac-notifications-core/src/main/java/org/siemac/metamac/notices/core/error/ServiceExceptionType.java
@@ -4,7 +4,7 @@ import org.siemac.metamac.core.common.exception.CommonServiceExceptionType;
 
 public class ServiceExceptionType extends CommonServiceExceptionType {
 
-    public static final CommonServiceExceptionType NOTIFICATION_NOT_FOUND           = create("exception.notifications.notification.not_found");
-    public static final CommonServiceExceptionType NOTIFICATION_ALREADY_EXISTS      = create("exception.notifications.notification.already_exists");
-    public static final CommonServiceExceptionType NOTIFICATION_RECEIVERS_NOT_FOUND = create("exception.notifications.notification.receivers_not_found");
+    public static final CommonServiceExceptionType NOTICE_NOT_FOUND           = create("exception.notices.notice.not_found");
+    public static final CommonServiceExceptionType NOTICE_ALREADY_EXISTS      = create("exception.notices.notice.already_exists");
+    public static final CommonServiceExceptionType NOTICE_RECEIVERS_NOT_FOUND = create("exception.notices.notice.receivers_not_found");
 }
