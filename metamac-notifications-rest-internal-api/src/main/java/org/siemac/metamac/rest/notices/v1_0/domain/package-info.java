006ce28760afce55842a03ac4b4802561df41731
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-05-20 06:44:54 +0000

    METAMAC-2294 - Extraer el proyecto api de notificaciones del proyecto multi-módulo >> Mover el propyecto api fuera del multi-módulo
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@52233 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-rest-internal-api/src/main/java/org/siemac/metamac/rest/notices/v1_0/domain/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/notices/v1.0/domain", xmlns = {@javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/notices/v1.0/domain", prefix = "notice")})
-package org.siemac.metamac.rest.notices.v1_0.domain;
-
