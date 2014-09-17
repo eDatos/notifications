e5191722664976b7e83e4374d3b83c11de72f438
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-09-17 11:15:12 +0000

    METAMAC-1984 - Aplicación de gestión de notificaciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@53282 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-notifications-web/src/main/java/org/siemac/metamac/notices/web/client/model/ds/NoticeDS.java
@@ -0,0 +1,28 @@
+package org.siemac.metamac.notices.web.client.model.ds;
+
+import com.smartgwt.client.data.DataSource;
+import com.smartgwt.client.data.fields.DataSourceTextField;
+
+public class NoticeDS extends DataSource {
+
+    public static final String NOTICE_ID            = "notice-id";
+    public static final String RECEIVER_ID          = "notice-receiver-id";
+    public static final String URN                  = "notice-urn";
+    public static final String SENDING_APPLICATION  = "notice-send-app";
+    public static final String SENDING_USER         = "notice-send-user";
+    public static final String EXPIRATION_DATE      = "notice-exp-date";
+    public static final String SUBJECT              = "notice-subject";
+    public static final String TYPE                 = "notice-type";
+    public static final String RECEIVER_USERNAME    = "notice-rec-username";
+    public static final String RECEIVER_ACKNOWLEDGE = "notice-rec-acknowledge";
+
+    public NoticeDS() {
+        DataSourceTextField noticeId = new DataSourceTextField(NOTICE_ID);
+        noticeId.setPrimaryKey(true);
+        addField(noticeId);
+
+        DataSourceTextField receiverId = new DataSourceTextField(RECEIVER_ID);
+        receiverId.setPrimaryKey(true);
+        addField(receiverId);
+    }
+}
