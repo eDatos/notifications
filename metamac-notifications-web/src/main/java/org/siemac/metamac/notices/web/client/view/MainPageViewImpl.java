7a6c9602f1d8d6d6a76983813bb30db5808f42d1
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-03-01 13:25:48 +0000

    METAMAC-2430 - Cambio general del layout de las aplicaciones
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-notifications/trunk/metamac-notifications@54873 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-notifications-web/src/main/java/org/siemac/metamac/notices/web/client/view/MainPageViewImpl.java
+++ metamac-notifications-web/src/main/java/org/siemac/metamac/notices/web/client/view/MainPageViewImpl.java
@@ -6,7 +6,6 @@ import org.siemac.metamac.notices.web.client.presenter.MainPagePresenter;
 import org.siemac.metamac.notices.web.client.view.handlers.MainPageUiHandlers;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
 import org.siemac.metamac.web.common.client.enums.MessageTypeEnum;
-import org.siemac.metamac.web.common.client.utils.ApplicationOrganisation;
 import org.siemac.metamac.web.common.client.widgets.BreadCrumbsPanel;
 import org.siemac.metamac.web.common.client.widgets.FooterLayout;
 import org.siemac.metamac.web.common.client.widgets.MasterHead;
@@ -78,7 +77,7 @@ public class MainPageViewImpl extends ViewWithUiHandlers<MainPageUiHandlers> imp
         southLayout = new HLayout();
         southLayout.setHeight100();
 
-        footerLayout = new FooterLayout(this.messagePanel, ApplicationOrganisation.getCurrentOrganisation(), NoticesWeb.getProjectVersion());
+        footerLayout = new FooterLayout(this.messagePanel, NoticesWeb.getProjectVersion());
 
         // Set user name
         masterHead.getUserNameLabel().setContents(getUserName());
